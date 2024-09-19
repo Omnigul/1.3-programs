a8983d53aeeb9e2fb050c2f0457483e89f04f20e
Omnigul <59460436+Omnigul@users.noreply.github.com>
2024-09-19 00:48:17 +0000

    changed program

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
-// determining amount of ingredint needed to make cookie(s) 
+// finding interest rate stuff or whatever :| 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
-double sugar = 1.5;
-double butter = 1;
-double flour = 2.75;
-double cookieNum;
+float principal;
+float rate;
+float compound;
+float balance;
+float interest;
 
 int main() 
 {
-  cout << "how many cookies do you want?" << endl;
-  cin >> cookieNum;
-  double sugarNum = cookieNum / 48 * sugar;
-  double butterNum = cookieNum / 48 * butter;
-  double flourNum = cookieNum / 48 * flour;
+  cout << "What is the principal?" << endl;
+  cin >> principal;
+  cout << "How much is the interest?" << endl;
+  cin >> rate;
+  cout << "How many times has the intrest compounded?" << endl;
+  cin >> compound;
 
-  cout << "you need " << sugarNum << " cups of sugar to make that (many) cookie(s)" << endl;
-  cout << "you need " << butterNum << " cups of butter to make that (many) cookie(s)" << endl;
-  cout << "you need " << flourNum << " cups of flour to make that (many) cookie(s)" << endl;
+  
+  interest = pow((1 + (rate/100) / compound),compound);
+  balance = principal * interest;
+
+  
+  cout << left << setw(15) << "Interest rate:" << right << setw(16) << rate << "%" << endl;
+  cout << left << setw(15) << "Times compounded:" << right << setw(15) << compound << endl;
+  cout << left << setw(15) << "Principal:" << right << setw(13) << "$" << principal << endl;
+  cout << setprecision(2) << fixed << left << setw(15) << "Interest:" << right << setw(12) << "$" << (interest - 1) * 1000 << endl;
+  cout << left << setw(15) << "Total balance:" << right << setw(10) << "$" << balance << endl;
 }
